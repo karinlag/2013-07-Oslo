73f51c6d37e4aeaab517a275063f0beabd48e50c
Karin Lagesen <karin.lagesen@gmail.com>
2013-06-03 20:42:49 +0200

    Removing python files

--- python-programming/namehandler2.py
+++
@@ -1,8 +0,0 @@
-def get_initials(full_name):
-   names = full_name.split()
-   initial = ''
-   i = 0
-   while i < len(names):
-      initial=initial+(names[i][0]) #From each part of the name we get the first letter
-      i = i+1
-   return initial
